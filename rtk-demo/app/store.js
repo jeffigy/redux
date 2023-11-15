308ecc4710bdab65560c87609b119b9a574a6fb0
jeffigy <ejeffydev@gmail.com>
2023-11-15 23:58:09 +0800

    addec icecream state feature

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,10 +1,11 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
-
+const icecreamReducer = require("../features/icecream/icecreamSlice");
 //* Store
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
+    icecream: icecreamReducer,
   },
 });
 
