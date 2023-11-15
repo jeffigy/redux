648bece2eb0eae59eeabd9747237cc1679271dda
jeffigy <ejeffydev@gmail.com>
2023-11-16 00:07:52 +0800

    added redux-logger to rtk

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,12 +1,17 @@
+const reduxLogger = require("redux-logger");
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
+
+const logger = reduxLogger.createLogger();
+
 //* Store
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
